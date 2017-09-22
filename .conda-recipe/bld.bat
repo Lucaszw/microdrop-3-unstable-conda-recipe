ef9c86ec208ac4929f3e495cebdffc1fc1da42dd
lucaszw <lucas.zeer@gmail.com>
2017-09-22 12:40:57 -0400

    moved npm install to end of conda build

--- .conda-recipe/bld.bat
+++ .conda-recipe/bld.bat
@@ -1,5 +1,4 @@
-npm install -g microdrop-3.0
-
 mkdir %PREFIX%\Scripts
 copy %RECIPE_DIR%\microdrop-3.ps1 %PREFIX%\Scripts
 copy %RECIPE_DIR%\microdrop-3.bat %PREFIX%\Scripts
+npm install -g microdrop-3.0
