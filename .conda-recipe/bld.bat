d8b9919ab78bbbf2230fe720e79c4baeed8a2816
lucaszw <lucas.zeer@gmail.com>
2017-09-22 12:22:08 -0400

    use PREFIX\Scripts

--- .conda-recipe/bld.bat
+++ .conda-recipe/bld.bat
@@ -1,5 +1,5 @@
 npm install -g microdrop-3.0
 
-mkdir %SCRIPTS%
-copy %RECIPE_DIR%\microdrop-3.ps1 %Scripts%
-copy %RECIPE_DIR%\microdrop-3.bat %Scripts%
+mkdir %PREFIX%\Scripts
+copy %RECIPE_DIR%\microdrop-3.ps1 %PREFIX%\Scripts
+copy %RECIPE_DIR%\microdrop-3.bat %PREFIX%\Scripts
