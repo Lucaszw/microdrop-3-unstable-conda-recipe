7a6cbe3830e8db0fa6b883dfb10ef10e9ac06f3c
lucaszw <lucas.zeer@gmail.com>
2017-10-02 10:00:36 -0400

    change to giturl

--- .conda-recipe/bld.bat
+++ .conda-recipe/bld.bat
@@ -1,4 +1,4 @@
 mkdir %PREFIX%\Scripts
 copy %RECIPE_DIR%\microdrop-3.ps1 %PREFIX%\Scripts
 copy %RECIPE_DIR%\microdrop-3.bat %PREFIX%\Scripts
-npm install -g microdrop-3.0
+npm install -g https://github.com/sci-bots/microdrop-3.0
